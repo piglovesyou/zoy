d478678059c31da6a9d74768932df65e6b08ba49
Takamura Soichi <takamura_s@ariel-networks.com>
2014-09-20 09:10:10 +0000

    append call in factory.soy

--- scaffold.sh
+++ scaffold.sh
@@ -6,23 +6,23 @@ do
     "i" ) INHERIT="$OPTARG" ;;
   esac
 done
-
 shift `expr $OPTIND - 1`
 
-# for TARGET in $@
-# do
-#     PATH=`echo $TARGET | sed -e 's/\./\//g'` 
-#     mkdir -p $PATH
-#     echo $PATH
-# done
+if [[ "$INHERIT" == "" || "$@" == "" ]]; then
+    echo USAGE: $ ./scaffold.sh -i goog.ui.InputDatePicker zoy.primitive.device.InputDatePicker
+    exit 1
+fi
 
 for TARGET in $@; do
 
-    P=$(echo $TARGET | sed -e 's/\./\//g') 
+    ################################
+    # Prepare directory
+    P=$(echo $TARGET | sed -e 's/\./\//g')
     B=$(basename $P)
     mkdir -p $P
-    
-    # JavaScript
+
+    ################################
+    # Generate JavaScript
     cat <<__EOT__ > $P/$B.js 2>&1
 goog.provide('$TARGET');
 
@@ -41,16 +41,17 @@ $TARGET = function(data) {
 };
 goog.inherits($TARGET, $INHERIT);
 __EOT__
-    # And require it
+    # Then require it
     if ! grep -Fq $TARGET ./zoy/all.js; then
         cat <<__EOT__ >> ./zoy/all.js
-            
+
 goog.require('$TARGET');
 goog.exportSymbol('$TARGET', $TARGET);
 __EOT__
     fi
 
-    # Soy
+    ################################
+    # Generate  Soy
     cat <<__EOT__ > $P/$B.soy 2>&1
 {namespace $TARGET.soy}
 
@@ -66,10 +67,26 @@ __EOT__
 {/template}
 __EOT__
 
-    # Stylus
+    # There must be an inserting point in factory.soy
+    INSERT_POINT='inserting point by scaffold.sh'
+    if ! grep -Fq "$INSERT_POINT" ./zoy/factory.soy; then
+        echo "There is no inserting point in scaffold.sh"
+        exit 1
+    fi
+
+    # Append calling new soy
+    if ! grep -Fq $TARGET ./zoy/factory.soy; then
+        INSERT_STRING="    {case '${TARGET}'} {call ${TARGET}.soy.main data=\"all\" /}"
+        echo $INSERT_POINT
+        sed -i "/${INSERT_POINT}/a  ${INSERT_STRING}" ./zoy/factory.soy
+    fi
+
+    ################################
+    # Generate Stylus
     cat <<__EOT__ > $P/$B.styl 2>&1
 // write style
 __EOT__
+    # Then import it
     STYLUS_PATH=$(echo $P | sed -e 's/^zoy\///g')
     if ! grep -Fq ${STYLUS_PATH} ./zoy/all.styl; then
         echo @import \'$STYLUS_PATH\' >> ./zoy/all.styl
